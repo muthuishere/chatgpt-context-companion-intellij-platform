fd7e3db0a000fcff5ad4af83279a7a47b238a731
GitHub Action <action@github.com>
2023-06-08 01:05:48 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "chatgpt-context-companion-intellij-platform"
--- src/main/kotlin/com/github/muthuishere/chatgptcontextcompanionintellijplatform/MyBundle.kt
+++ src/main/kotlin/com/github/muthuishere/chatgptcontextcompanionintellijplatform/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.muthuishere.chatgptcontextcompanionintellijplatform
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
